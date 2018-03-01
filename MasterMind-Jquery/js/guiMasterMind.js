d6c1d8a075428ba3368d09d1058d42605e281000
Javier Ponferrada López <jponferrada26@gmail.com>
2018-03-01 12:00:54 +0100

    cambios MasterMind-jquery

--- MasterMind-Jquery/js/guiMasterMind.js
+++ MasterMind-Jquery/js/guiMasterMind.js
@@ -1,48 +1,14 @@
-{
-    let $coloresAElegir,
-        $arrayColoresSeleccionados,
-        $colorPulsado,
-        $containerIntentos,
-        $colorElegidoIntento,
-        $btnCheck,
-        $arrayIntentoAComprobar,
-        $numIntentos,
-        $containerLeft,
-        $containerUltimoIntento,
-        $btnStart,
-        $btnSalir,
+
+    let $numIntentos,
         $formularioMenu,
         $backgroundMenu,
         $tituloResultadoMenuPartidaMasterMind,
         $numeroIntentosResultadoMenuPartidaMasterMind;
 
-
     let getUltimoIntento = function () {
-        for (let i = $containerIntentos.children().length; i > 0; i--) {
-            if ($containerIntentos.children()[i - 1].nodeName != '#text') {
-                return $containerIntentos.children()[i - 1];
-            }
-        }
-
+        return $('.containerIntentoColores:last-child');
     };
-
-    let insertarColorUltimoIntento = function () {
-        let coloresUltimoIntento = $('.circleIntentoColor', $containerUltimoIntento);
-        coloresUltimoIntento.each(function (index, color) {
-            color = $(color);
-            if (!color.hasClass('colorRelleno')) {
-                $padding = color.css('padding').replace('px', '');
-                color.animate({
-                    padding: ($padding + 5) + 'px'
-                },500);
-                color.css('background', $colorPulsado);
-                color.addClass('colorRelleno');
-                return false;
-            }
-        });
-
-    };
-
+    
     let getColor = function (rgb) {
         switch (rgb) {
             case 'rgb(255, 34, 34)':
@@ -61,42 +27,16 @@
                 return 'azul';
             case 'rgb(34, 255, 89)':
                 return 'verde';
-
-        }
-    };
-
-    let insertColorEstadoUltimoIntento = function (color, posicion) {
-        let classCirculo = $('.circleEstadoColor', $containerUltimoIntento)[posicion];
-        $(classCirculo).css('background', color);
-    };
-
-    let drawIntentoColorEnSuSitio = function (numeroColorEnSuSitio, posicion) {
-        for (let i = 0; i < numeroColorEnSuSitio; i++) {
-            insertColorEstadoUltimoIntento('#000', posicion++);
-        }
-        return posicion;
-
-    };
-
-    let drawIntentoColorEsta = function (numeroColorEsta, posicion) {
-        for (let i = 0; i < numeroColorEsta; i++) {
-            insertColorEstadoUltimoIntento('#FFF', posicion++);
         }
     };
 
     let getNumeroColoresInsertados = function () {
-        return $('.colorRelleno', $containerUltimoIntento).length;
+        return $('.colorRelleno', getUltimoIntento()).length;
     };
 
-    /**
-     * Nuevo intento
-     */
     let nuevoIntento = function () {
-        let ultimoIntentoAnterior = getUltimoIntento();
-        if (ultimoIntentoAnterior != undefined) { //comprobar si existe un último intento
-            $(getUltimoIntento()).css('pointerEvents', 'none');
-        }
-        $containerIntentos.append('<div class="containerIntentoColores">' +
+        $(getUltimoIntento()).css('pointerEvents', 'none');
+        $('#containerIntento').append('<div class="containerIntentoColores">' +
             '<div class="circleIntentoColor"></div>' +
             '<div class="circleIntentoColor"></div>' +
             '<div class="circleIntentoColor"></div>' +
@@ -110,120 +50,59 @@
         mastermind.incrementarIntentos();
     };
 
-    /**
-     * Comprobar intento
-     */
     let clickCheckIntento = function () {
         if (getNumeroColoresInsertados() == 4) {
-            $arrayIntentoAComprobar = [];
-            let coloresIntentoAComprobar = $('.circleIntentoColor', $containerUltimoIntento);
-            coloresIntentoAComprobar.each(function (index, element) {
+            let $arrayIntentoAComprobar = [];
+            $('.circleIntentoColor', getUltimoIntento()).each(function (index, element) {
                 $arrayIntentoAComprobar.push(getColor($(element).css('background-color')));
             });
-
             let resultadoMasterMind = mastermind.comprobarEstado($arrayIntentoAComprobar);
-
-            let posicion = 0;
-            posicion = drawIntentoColorEnSuSitio(resultadoMasterMind.colorEnSuSitio, posicion);
-            drawIntentoColorEsta(resultadoMasterMind.colorEsta, posicion);
+            $($('.circleEstadoColor', getUltimoIntento())).slice(0,resultadoMasterMind.colorEnSuSitio).css('background', '#000');
+            $($('.circleEstadoColor', getUltimoIntento())).slice(resultadoMasterMind.colorEnSuSitio,resultadoMasterMind.colorEsta+resultadoMasterMind.colorEsta).css('background', '#FFF');
+    
             if (resultadoMasterMind.colorEnSuSitio != 4) {
                 nuevoIntento();
-                $containerUltimoIntento = getUltimoIntento();
                 updateContadorIntentos();
                 iniciarCalbackSeleccionColorIntento();
                 goToScrollBottom();
-
             } else {
                 viewMenu(true);
             }
-
         }
-
     };
 
+    let iniciarCalbackSeleccionColorIntento = function(){
+        $('.circleIntentoColor', getUltimoIntento()).on('click', clickcolorElegidoIntento);
+    };
 
-    /**
-     * 
-     */
     let clickBotonElegir = function () {
-        $colorPulsado = $(this).css('background');
-        insertarColorUltimoIntento();
+        $($('.circleIntentoColor:not([class="circleIntentoColor colorRelleno"])', getUltimoIntento())[0])
+        .css('background', $(this).css('background'))
+        .addClass('colorRelleno')
+        .animate({
+            padding: '5px'
+        }, 500)
+        .animate({
+            padding: '0px'
+        }, 500);
     };
 
     let clickcolorElegidoIntento = function () {
-        let elementoPulsado = $(this);
-        if (elementoPulsado.hasClass('colorRelleno')) {
-            elementoPulsado.removeClass('colorRelleno');
-        }
-
-        $padding = elementoPulsado.css('padding').replace('px', '');
-        elementoPulsado.animate({
-            padding: ($padding - 5) + 'px'
-        },500);
-        
-        elementoPulsado.css('background', '#d6d6d6fa');
-
-    }
-
-
-    let iniciarCalbackColoresElegir = function () {
-        $coloresAElegir.on('click', clickBotonElegir);
+        $(this).removeClass('colorRelleno').css('background', '#d6d6d6fa');
     };
 
-    /**
-     * Asignar los calback al a los colores de cada intento
-     */
-    let iniciarCalbackSeleccionColorIntento = function () {
-        $colorElegidoIntento = $('.circleIntentoColor', $containerUltimoIntento);
-        $colorElegidoIntento.on('click', clickcolorElegidoIntento);
-    };
-
-    /**
-     * Asignar los calback al botón de chequear.
-     */
-    let iniciarCalbackBtnCheck = function () {
-        $btnCheck.on('click', clickCheckIntento);
-    };
-
-    /**
-     * Asignar los calback al botón de Nueva partida.
-     */
-    let inidicarCalbackBtnStart = function () {
-        $btnStart.on('click', start);
-    };
-
-    /**
-     * Asignar los calback al botón de Salir.
-     */
-    let inidicarCalbackBtnSalir = function () {
-        $btnSalir.on('click', salir);
-    };
-
-
-    /**
-     * Actualizar el contador de intentos
-     */
     let updateContadorIntentos = function () {
         $numIntentos.html('Intentos: ' + mastermind.getNumIntentos());
     };
 
-    /**
-     * Ir al final del scroll del contenedor que almacena los intentos.
-     */
     let goToScrollBottom = function () {
-        $containerLeft.scrollTop($containerLeft.scrollHeight);
+        $('#containerLeft').scrollTop($(this).height());
     };
 
-    /**
-     * Salir de la partida
-     */
     let salir = function () {
         window.close();
     };
 
-    /**
-     * Mostrar Menu
-     */
     let viewMenu = function () {
         if (arguments[0] == true) { //comprobar si existe un argumento con true que significa que tiene que ha ganado la partida
             $tituloResultadoMenuPartidaMasterMind.html('¡Has Ganado!');
@@ -236,9 +115,6 @@
         });
     };
 
-    /**
-     * Cerrar menu
-     */
     let closeMenu = function () {
         $formularioMenu.css('top', '-200%');
         $backgroundMenu.css({
@@ -247,61 +123,26 @@
         });
     };
 
-    let clearAllIntententos = function () {
-        $containerIntentos.empty();
-        /*while ($containerIntentos.firstChild) {
-            $containerIntentos.removeChild($containerIntentos.firstChild);
-        }
-        */
-        nuevoIntento();
-
-    };
-
-    let actualizar$containerIntentos = function () {
-        $containerIntentos = $('#containerIntento');
-    };
-
-
-    /**
-     * Empezar nueva partida
-     */
     let start = function () {
         closeMenu();
-
         mastermind.init();
         mastermind.mostrar();
-
-        $coloresAElegir = $('.colorEleccion');
-        $containerIntentos = $('#containerIntento');
-        clearAllIntententos();
-        $arrayColoresSeleccionados = [];
-        $containerLeft = $('#containerLeft');
+        $('#containerIntento').empty();
+        nuevoIntento();
         $numIntentos = $('#contadorIntentos');
-        $btnCheck = $('#btn-checkColores');
-
-        $containerUltimoIntento = getUltimoIntento();
-        iniciarCalbackColoresElegir();
+        $('#btn-checkColores').on('click', clickCheckIntento);       
         iniciarCalbackSeleccionColorIntento();
-        iniciarCalbackBtnCheck();
         updateContadorIntentos();
         goToScrollBottom();
     };
 
-
-    /*-------Init---------*/
-
     let init = function () {
-        $btnStart = $('#btn-Start');
-        $btnSalir = $('#btn-Salir');
+        $('#btn-Start').on('click', start);
+        $('#btn-Salir').on('click', salir);
         $formularioMenu = $('#containerMenuOpciones');
         $backgroundMenu = $('#backgroundMenuOpciones');
         $tituloResultadoMenuPartidaMasterMind = $('#titleResultadoMenuPartida');
         $numeroIntentosResultadoMenuPartidaMasterMind = $('#numeroIntentosMenuPartida');
-        inidicarCalbackBtnStart();
-        inidicarCalbackBtnSalir();
-
+        $('.colorEleccion').on('click', clickBotonElegir);
     };
-
-    $(window).ready(init);
-
-}
+    $(init);
